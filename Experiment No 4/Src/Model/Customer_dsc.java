b0bed5f1755a8db4fd32028564684b529a8da3e6
abhiq3a7r8 <140839210+abhiq3a7r8@users.noreply.github.com>
2023-09-10 11:53:21 +0530

    Delete Experiment No 4/Src directory

--- Experiment No 4/Src/Model/Customer_dsc.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface Customer_dsc {
-     void addToCart(String product);
-     void removeFromCart(String product);
-     void viewCart();
-}
--- Experiment No 4/Src/Model/Electronic_Device_dsc.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public interface Electronic_Device_dsc {
-    void turnOn();
-    void turnOff();
-    boolean isTurnedOn();
-    void setTimer(int minutes);
-}
-
