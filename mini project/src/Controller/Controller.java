d02dc94fb84bebfd985293aee224237e77908e05
abhiq3a7r8 <audire444@gmail.com>
2023-10-14 10:30:28 +0530

    customer scrolling fixed

--- mini project/src/Controller/Controller.java
+++ mini project/src/Controller/Controller.java
@@ -18,27 +18,37 @@ public class Controller {
         view = v;
         view.centerInitialSetup(model.getManageElectronicDeviceData().getLinesBeingDisplayed(), model.getManageElectronicDeviceData().getHeaders().size());
         view.centerUpdate(model.getManageElectronicDeviceData().getLines(model.getManageElectronicDeviceData().getFirstLineToDisplay(), model.getManageElectronicDeviceData().getLastLineToDisplay()), model.getManageElectronicDeviceData().getHeaders());
-        addScrolling();
-        addButtonClick();
-    }
-    private void addButtonClick()
-    {
-        view.getMf().getIp().getBp().getBtn_Customer().addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent me) {
-                System.out.println("Customer Pressed");
+        view.centerInitialSetup(model.getManageCustomerData().getLinesBeingDisplayed(), model.getManageCustomerData().getHeaders().size());
+        view.centerUpdate(model.getManageCustomerData().getLines(model.getManageCustomerData().getFirstLineToDisplay(), model.getManageCustomerData().getLastLineToDisplay()), model.getManageCustomerData().getHeaders());
 
-                view.centerUpdate(model.getManageCustomerData().getLines(model.getManageCustomerData().getFirstLineToDisplay(), model.getManageCustomerData().getLastLineToDisplay()), model.getManageCustomerData().getHeaders());
-            }
-        });
+
+
+        addButtonClickDevices();
+        addButtonClickCustomers();
+
+    }
+    private void addButtonClickDevices()
+    {
 
         view.getMf().getIp().getBp().getBtn_Electronic_Device().addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent me) {
                 System.out.println("Electronic Device Pressed");
+                addScrollingDevices();
                 view.centerUpdate(model.getManageElectronicDeviceData().getLines(model.getManageElectronicDeviceData().getFirstLineToDisplay(), model.getManageElectronicDeviceData().getLastLineToDisplay()), model.getManageElectronicDeviceData().getHeaders());
             }
         });
     }
-    private void addScrolling()
+
+    private void addButtonClickCustomers(){
+        view.getMf().getIp().getBp().getBtn_Customer().addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent me) {
+                System.out.println("Customer Pressed");
+                addScrollingCustomers();
+                view.centerUpdate(model.getManageCustomerData().getLines(model.getManageCustomerData().getFirstLineToDisplay(), model.getManageCustomerData().getLastLineToDisplay()), model.getManageCustomerData().getHeaders());
+            }
+        });
+    }
+    private void addScrollingDevices()
     {
         view.getMf().getIp().getCp().addMouseWheelListener(new MouseWheelListener() {
             @Override
@@ -84,6 +94,55 @@ public class Controller {
                 }
 
                 view.centerUpdate(model.getManageElectronicDeviceData().getLines(model.getManageElectronicDeviceData().getFirstLineToDisplay(), model.getManageElectronicDeviceData().getLastLineToDisplay()), model.getManageElectronicDeviceData().getHeaders());
+            }
+        });
+    }
+    private void addScrollingCustomers()
+    {
+        view.getMf().getIp().getCp().addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                int units = e.getUnitsToScroll();
+                System.out.println(units);
+                int current_first_line = model.getManageCustomerData().getFirstLineToDisplay();
+                int current_last_line = model.getManageCustomerData().getLastLineToDisplay();
+                int no_of_players = model.getManageCustomerData().getTable().size();
+                int no_of_display_lines = model.getManageCustomerData().getLinesBeingDisplayed();
+                if(units <= 0 && current_first_line == 0)
+                {
+                    model.getManageCustomerData().setFirstLineToDisplay(0);
+                }
+                else if(units <= 0 && current_first_line > 0)
+                {
+                    int new_first_line = current_first_line + units;
+                    if(new_first_line <= 0)
+                    {
+                        model.getManageCustomerData().setFirstLineToDisplay(0);
+                    }
+                    else
+                    {
+                        model.getManageCustomerData().setFirstLineToDisplay(new_first_line);
+                    }
+                }
+                else if(units > 0 && current_last_line == no_of_players-1)
+                {
+                    model.getManageCustomerData().setFirstLineToDisplay(current_first_line);
+                }
+                else if (units > 0 && current_last_line < no_of_players-1)
+                {
+                    int new_first_line = current_first_line + units;
+                    if(new_first_line > no_of_players - no_of_display_lines)
+                    {
+                        new_first_line = no_of_players-no_of_display_lines;
+                        model.getManageCustomerData().setFirstLineToDisplay(new_first_line);
+                    }
+                    else
+                    {
+                        model.getManageCustomerData().setFirstLineToDisplay(new_first_line);
+                    }
+                }
+
+                view.centerUpdate(model.getManageCustomerData().getLines(model.getManageCustomerData().getFirstLineToDisplay(), model.getManageCustomerData().getLastLineToDisplay()), model.getManageCustomerData().getHeaders());
             }
         });
     }
